a2eb867aa98da0a9dc1288eacac6264b29fbcffb
MatiasMy <ma  git config --global user.name Matias>
2023-08-17 10:24:35 +0300

    kirjautuminen valmis

--- news_2021/controllers/userManagement.php
+++ news_2021/controllers/userManagement.php
@@ -59,3 +59,4 @@ function logoutController(){
     header("Location: /login"); // forward eli uudelleenohjaus
     die();
 }
+
--- news_2021/views/login.view.php
+++ news_2021/views/login.view.php
@@ -14,4 +14,5 @@
 </form>
 </div>
 
+
 <?php require "partials/footer.php"; ?>
